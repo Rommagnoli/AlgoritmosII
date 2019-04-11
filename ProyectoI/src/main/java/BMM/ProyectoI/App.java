45a29dc0130dc54293d26e94f8a030a24a47b0d8
boaglio15 <agustinboaglio.95@gmail.com>
2019-04-11 18:26:55 -0300

    Agregado main y correccion de pom

--- ProyectoI/src/main/java/BMM/ProyectoI/App.java
+++ ProyectoI/src/main/java/BMM/ProyectoI/App.java
@@ -4,10 +4,11 @@ package BMM.ProyectoI;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String s = "romanagustin hola josue";
+        System.out.println(StringMatching.match(s,"hola"));
     }
 }
