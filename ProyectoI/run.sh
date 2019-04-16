bfd25bf203afa9e1e80ae2e3d00fe40ecb558612
Rommagnoli <roman_magnoli@hotmail.com>
2019-04-16 17:29:14 -0300

    Correcion de la estructura del proyecto

---
+++ ProyectoI/run.sh
@@ -0,0 +1,11 @@
+echo "*******************  BUILDING MODULE  *****************************************"
+mvn clean install
+
+echo "*******************  COLLECTING DEPENDENCIES  *********************************"
+mvn dependency:copy-dependencies
+export CLASPATH=""
+for file in `ls target/dependency`; do export CLASSPATH=$CLASSPATH:target/dependency/$file; done
+export CLASSPATH=$CLASSPATH:target/classes
+
+echo "*******************  EXECUTING PROGRAM ******************************************"
+java ProyectoI.src.main.java.BMM.ProyectoI.App
