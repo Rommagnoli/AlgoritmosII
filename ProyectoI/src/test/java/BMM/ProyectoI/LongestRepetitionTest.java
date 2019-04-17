f139ce341947ca16a872cbf06e1a89d084672838
boaglio15 <agustinboaglio.95@gmail.com>
2019-04-17 19:45:10 -0300

    Nuevos Tests

--- ProyectoI/src/test/java/BMM/ProyectoI/LongestRepetitionTest.java
+++ ProyectoI/src/test/java/BMM/ProyectoI/LongestRepetitionTest.java
@@ -5,11 +5,19 @@ import static org.junit.Assert.*;
 
 public class LongestRepetitionTest {
 
+    //Tests para corroborar el funcionamiento del metodo cadRepeat de la clase LongestRepetition.
     @Test
     public void testCadRepetidas() {
         String prueba = "asdholaasdmholasphola";
         String res = LongestRepetition.cadRepetidas(LongestRepetition.subcadenas(prueba));
         assertEquals("hola", res);
+    }
+
+    @Test
+    public void testCadRepetidasNotRepeat() {
+        String prueba = "abcdefghijklmnopqrst";
+        String res = LongestRepetition.cadRepetidas(LongestRepetition.subcadenas(prueba));
+        assertEquals("", res);
     }
 
 }
--- ProyectoI/src/test/java/BMM/ProyectoI/StringMatchingTest.java
+++ ProyectoI/src/test/java/BMM/ProyectoI/StringMatchingTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 
 public class StringMatchingTest{
 
-    //Test para corroborar el funcionamiento del metodo match de la clase StringMatching.
+    //Tests para corroborar el funcionamiento del metodo match de la clase StringMatching.
     @Test
     public void testMatchCadenas(){
         String text = "dsfaksvmsidododfsasd";
@@ -14,12 +14,28 @@ public class StringMatchingTest{
         assertEquals(10,res);
     }
 
-    //Test para corroborar el funcionamiento del metodo matchKmp de la clase StringMatching.
+    @Test
+    public void testMatchCadenasNotFound(){
+        String text = "dsfaksvmsidododfsasd";
+        String cadMatch = "hola";
+        int res = StringMatching.match(text,cadMatch);
+        assertEquals(-1,res);
+    }
+
+    //Tests para corroborar el funcionamiento del metodo matchKmp de la clase StringMatching.
     @Test
     public void testMatchCadenasKmp(){
         String text = "aswqqeavooewzaratewqxcvsa";
         String cadMatch = "zarate";
         int res = StringMatching.matchKmp(text,cadMatch);
         assertEquals(12,res);
+    }
+
+    @Test
+    public void testMatchCadenasKmpNotFound(){
+        String text = "aswqqeavooewzaratewqxcvsa";
+        String cadMatch = "zarati";
+        int res = StringMatching.matchKmp(text,cadMatch);
+        assertEquals(-1,res);
     }
 }
